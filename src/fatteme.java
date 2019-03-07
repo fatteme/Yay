ebd815b14a51c34f33f518ece172af6c0c5aa94d
ArshiA <a_akhavan@outlook.com>
2019-03-07 10:49:28 +0330

    "yeki az azaye goruh main ro avaz kone va push kone" said hamed the git god

--- src/fatteme.java
+++ src/fatteme.java
@@ -1,2 +1,9 @@
+import java.util.Scanner;
+
 public class fatteme {
+    public static void main(String[] args) {
+        do {
+            System.out.println("hy im fatteme im new to this things");
+        } while (new Scanner(System.in).hasNext());
+    }
 }
