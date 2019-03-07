d14486a79a662525b41dbd7ee8ef940c355aced2
fateme <fatt3me@gmail.com>
2019-03-07 10:28:54 +0330

    changed Main: added SHUTUP

--- src/ArshiA.java
+++ src/ArshiA.java
@@ -3,6 +3,7 @@ public class ArshiA {
         System.out.println("hey ArshiA this project is made for you to learn how to work with git");
         System.out.println("hey fatteme first of all heh");
         System.out.println("and second of all nothing im done");
+        System.out.println("ArshiA shut up");
     }
 }
 /*
