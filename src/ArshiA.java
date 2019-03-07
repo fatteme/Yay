ae731ec2055e1bc6d3f5f3761c281ee5c4b9a01a
ArshiA <a_akhavan@outlook.com>
2019-03-07 10:14:14 +0330

    GitIgnore

--- src/ArshiA.java
+++ src/ArshiA.java
@@ -5,3 +5,63 @@ public class ArshiA {
         System.out.println("and second of all nothing im done");
     }
 }
+/*
+* i was five and she was 6
+* we would road on horses made of sticks
+* she wore black and i wore white
+* she would always win the fight
+* bang bang
+* she shot me down
+* bang bang
+* i hit the ground bang bang
+* that awful sound bang bang
+* my baby shot me down
+*
+*
+*
+*
+*
+*
+*
+* season came and changed the time
+* when i grew up i called her mine
+* she would always laugh and say
+* remember when we used to play
+* bang bang
+* you shot me down bang bang
+* i hit the ground bang bang
+* that awful sound bang bang
+* you used to shoot me dowwwwwwn
+*
+*
+*
+*
+*
+*
+*
+* music played and people sang
+* just for me the churche bell rang
+* newnewnewenew
+* newnewnewnwew
+* b
+* a
+* a
+* a
+* a
+* n
+* n
+* n
+* g
+* g
+* g
+* g
+* now she is gone i dont know why
+* until this time some times i cried
+* she didnt even say good bye
+* she didnt take the time to hide
+* bang bang
+* she shot me down bang bang
+* that awful sound bang bang
+* i hit the ground bang bang
+* my baby shot me dowwwwwwwwwn
+*/
