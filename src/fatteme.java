1bfa81b7c9bc77ddb875c359b64756a6d97b4eac
ArshiA <a_akhavan@outlook.com>
2019-03-07 11:37:11 +0330

    PushBranch

--- src/fatteme.java
+++ src/fatteme.java
@@ -2,5 +2,6 @@ public class fatteme {
     public static void main(String[] args) {
         System.out.println("some body please shut me up");
         System.out.println("shutup mehrdad");
+        System.out.println("ArshiA is every where");
     }
 }
