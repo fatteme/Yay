dbeb0efc1aeceeeafb4a7e78f06a63bf501f7e56
ArshiA <a_akhavan@outlook.com>
2019-03-07 09:47:10 +0330

    Fatteme hates me

--- src/ArshiA.java
+++ src/ArshiA.java
@@ -1,5 +1,7 @@
 public class ArshiA {
     public static void main(String[] args) {
         System.out.println("hey ArshiA this project is made for you to learn how to work with git");
+        System.out.println("hey fatteme first of all heh");
+        System.out.println("and second of all nothing im done");
     }
 }
